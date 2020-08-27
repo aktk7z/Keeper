2ee0777711cea74da916473b1465d59fb408c885
x1a09i <x1a09i.dev@gmail.com>
2020-08-27 23:38:12 +0900

    Implement props and data mapping

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-const Note = () => {
-  const title = "This is the note title";
-  const content = "This is the note content";
+const Note = (props) => {
   return (
     <div className="note">
-      <h1>{title}</h1>
-      <p>{content}</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 };
